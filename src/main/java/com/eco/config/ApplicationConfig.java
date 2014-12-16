1ec7844892c8b850d1b3fd528a936e6c983e3952
sushil-dosi <sushildosi@gmail.com>
2014-12-15 18:30:18 -0700

    moving config to separate file

--- src/main/java/com/eco/config/ApplicationConfig.java
+++ src/main/java/com/eco/config/ApplicationConfig.java
@@ -6,9 +6,12 @@ import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -23,9 +26,13 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories("com.eco.repositories")
+@PropertySource({ "classpath:persistence-mysql.properties" })
 @ComponentScan(basePackages = {"com.eco"})
 public class ApplicationConfig{
  
+   @Autowired
+   private Environment env;	
+	
    @Bean
    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
       LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
@@ -52,10 +59,10 @@ public class ApplicationConfig{
    @Bean 
    public DataSource dataSource(){
 	   BasicDataSource dataSource = new BasicDataSource();
-	   dataSource.setUrl("jdbc:mysql://127.0.0.1:8889/testEco");
-	   dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-	   dataSource.setUsername( "root" );
-	   dataSource.setPassword( "root" );
+	   dataSource.setUrl(env.getProperty("jdbc.url"));
+	   dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+	   dataSource.setUsername( env.getProperty("jdbc.user") );
+	   dataSource.setPassword( env.getProperty("jdbc.pass") );
 	   dataSource.setRemoveAbandoned(true);
 	   dataSource.setInitialSize(20);
 	   dataSource.setMaxActive(30);
@@ -83,8 +90,8 @@ public class ApplicationConfig{
    Properties additionalProperties() {
       Properties properties = new Properties();
       //properties.setProperty("hibernate.hbm2ddl.auto", "create");
-      properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-      properties.setProperty("hibernate.show_sql", "true");
+      properties.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+      properties.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
       return properties;
    }
 }
